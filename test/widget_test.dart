00a2b37b365437875532d75ebff1631d8e1e6bd6
nishuR31 <nishangithub@gmail.com>
2026-04-29 23:01:58 +0530

    lollll

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,6 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:boardvault/main.dart';
+import 'package:boardvault/app/index.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
